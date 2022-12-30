dee997e392d17f736995093674dc67164a08941f
武晓慧 <374620777@qq.com>
2022-12-30 10:18:22 +0800

    项目初始化完毕

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 覆盖的是端口号
+  // 替换完毕项目重启
+  //   ctrl+c 终止掉项目   yarn serve重新启动
+  devServer: {
+    port: 3001
+  }
 })
